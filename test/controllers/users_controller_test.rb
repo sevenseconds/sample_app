feab3bebaaf7d5986711f691a97adfbb419256d6
Seven Seconds <sevenseconds@gmail.com>
2017-11-25 22:06:26 +0700

    Add admin field

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,14 +30,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect edit when logged in as wrong user' do
-    log_in_as(@other_user)
+    log_in_as@other_user
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
   test 'should redirect update when logged in as wrong user' do
-    log_in_as(@other_user)
+    log_in_as@other_user
     patch user_path(@user), params: {
         user: {
             name: @user.name,
@@ -51,6 +51,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as @other_user
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: {
+            password: 'password',
+            password_confirmation: 'password',
+            admin: true
+        }
+    }
+    assert_not @other_user.reload.admin?
   end
 
 end
